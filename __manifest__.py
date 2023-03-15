547ad7a21e862ba476b83a9272120f620971f5be
Hafeesulali <hafeesulali@gmail.com>
2023-03-15 12:55:02 +0530

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'SO Approval',
+    'version': '16.0.1.0.0',
+    'depends': ['sale', 'base'],
+    'data': [
+        'views/so_approval_views.xml',
+        'views/so_menuitem_views.xml',
+    ],
+    'installable': True,
+}
